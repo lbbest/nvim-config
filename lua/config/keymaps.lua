4c8c15f62c12658f9c513c367a30804aa73a1acf
Liam Best <59018845+lbbest@users.noreply.github.com>
2026-02-01 16:20:41 +0100

    feat: add file grep keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Telescope
 local builtin = require('telescope.builtin')
+-- Ctrl + p
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+-- Ctrl + f
+vim.keymap.set('n', '<C-f>', builtin.live_grep, { desc = 'Telescope grep files' })
