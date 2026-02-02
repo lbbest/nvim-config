d192c20c2d0c70976f8db77af454a4fa37f4ea56
Liam Best <59018845+lbbest@users.noreply.github.com>
2026-02-02 13:16:26 +0100

    feat: show neotree when opening directory

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,4 +9,15 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   lazy = false, 
+  config = function()
+    -- Automatically show Neotree if opening directory
+    vim.api.nvim_create_autocmd('VimEnter', {
+      callback = function(data)
+        local is_directory = vim.fn.isdirectory(data.file) == 1
+        if (is_directory or vim.fn.argc() == 0) then
+          vim.cmd("Neotree show")
+        end
+      end
+    })
+  end
 }
